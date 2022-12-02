c4d396e04046ca14b4b50f0f6748a4e07a9e16e4
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2022-12-02 15:19:40 +0600

    my commit

--- starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class SareetaApplication {
 
 	public static void main(String[] args) {
+		// spring apps starts here
 		SpringApplication.run(SareetaApplication.class, args);
 	}
 
